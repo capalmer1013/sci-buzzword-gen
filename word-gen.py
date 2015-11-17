3eab8d08253af64798774ac72f754a876a6599d0
Chris Palmer <capalmer1013@gmail.com>
2015-11-17 11:40:52 -0500

    formatting and indexing
    fixed newlines and indexing

--- word-gen.py
+++ word-gen.py
@@ -10,13 +10,13 @@ for line in f:
 	if line == "!\n":
 		secondWord = True
 	elif secondWord:
-		secondList.append(line)
+		secondList.append(line.rstrip())
 	else:
-		firstList.append(line)
-firstIndex = random.randint(0, len(firstList))
-secondIndex = random.randint(0, len(secondList))
+		firstList.append(line.rstrip())
+firstIndex = random.randint(0, len(firstList)-1)
+secondIndex = random.randint(0, len(secondList)-1)
 
-if firstList[firstIndex] != "theory\n" and secondList[secondIndex] != "theory\n":
+if firstList[firstIndex] != "theory" and secondList[secondIndex] != "theory":
 	print firstList[firstIndex] + ' ' + secondList[secondIndex] + " theory"
 else:
 	print firstList[firstIndex] + ' ' + secondList[secondIndex]
